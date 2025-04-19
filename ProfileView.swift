f9ee50a2d9b38b4689057298288e6e025577e0d6
Terrance <terrancehah@gmail.com>
2025-04-19 23:04:02 +0800

    Add onboarding flow with user preferences model and category selection

---
+++ ProfileView.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+// ProfileView.swift
+// Shows and allows editing of user profile info.
+
+struct ProfileView: View {
+    @ObservedObject var userProfile: UserProfileViewModel
+    var body: some View {
+        Form {
+            Section(header: Text("Account Info")) {
+                Text("Name: \(userProfile.user.name)")
+                Text("Email: \(userProfile.user.email)")
+            }
+            Section {
+                Button("Edit Profile") {
+                    // TODO: Implement edit profile
+                }
+                Button("Logout") {
+                    // TODO: Implement logout
+                }
+            }
+        }
+        .navigationTitle("Profile")
+    }
+}
+
+#if DEBUG
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView(userProfile: UserProfileViewModel.mock)
+    }
+}
+#endif
